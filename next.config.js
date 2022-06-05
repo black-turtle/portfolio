00e83b72be9ba0097fa64be3c99028f3de9ace43
black-turtle <jony.pilo.10@gmail.com>
2022-06-05 19:17:30 +0600

    update deploy scripts

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const prod = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
+  'process.env.BACKEND_URL': prod
+    ? 'https://black-turtle.github.io/portfolio'
+    : '',
 };
 
 module.exports = nextConfig;
