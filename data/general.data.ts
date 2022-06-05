1121a6a8f6eb1728f6701f26e899a70438a162b6
black-turtle <jony.pilo.10@gmail.com>
2022-06-05 19:33:12 +0600

    remove out folder

--- data/general.data.ts
+++ data/general.data.ts
@@ -2,7 +2,7 @@ import { GeneralDataType } from './types';
 
 export const generalData: GeneralDataType = {
   portFolioTitle: `Khairul's portfolio`,
-  name: `Test`,
+  name: `Md Khairul Islam`,
   role: `Full-stack / Backend developer`,
   personalEmail: `jonys.mailbox@gmail.com`,
   linkedInUrl: `https://www.linkedin.com/in/md-khairul-islam-jony`,
