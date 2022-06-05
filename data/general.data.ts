abbc4ef29d701366db49fa2a96753d1678b3cb15
black-turtle <jony.pilo.10@gmail.com>
2022-06-05 19:30:38 +0600

    test changes

--- data/general.data.ts
+++ data/general.data.ts
@@ -2,7 +2,7 @@ import { GeneralDataType } from './types';
 
 export const generalData: GeneralDataType = {
   portFolioTitle: `Khairul's portfolio`,
-  name: `Md Khairul Islam`,
+  name: `Test`,
   role: `Full-stack / Backend developer`,
   personalEmail: `jonys.mailbox@gmail.com`,
   linkedInUrl: `https://www.linkedin.com/in/md-khairul-islam-jony`,
